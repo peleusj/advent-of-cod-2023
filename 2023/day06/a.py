4a9b9b8c80eb5ed00355e5a0b5d8aee64b5e0307
Albert Ji <peleusj@gmail.com>
2024-11-12 10:15:32 +0800

    solve in quadratic

--- 2023/day06/a.py
+++ 2023/day06/a.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 with open(sys.argv[1]) as file:
@@ -13,7 +14,7 @@ p2_time = int("".join(times))
 p2_distance = int("".join(distances))
 
 
-def race(time_limit, record_distance):
+def count_brutal(time_limit, record_distance):
     result = 0
     for t in range(time_limit + 1):
         if (time_limit - t) * t > record_distance:
@@ -21,11 +22,28 @@ def race(time_limit, record_distance):
     return result
 
 
+def count_quadratic(t, d):
+    """
+    Time:      7  15   30
+    Distance:  9  40  200
+    (t - h) * h  - d > 0
+    -h^2 + th - d > 0
+    """
+    a = -1
+    b = t
+    c = -d
+    root1 = math.ceil((-b + pow(b * b - 4 * a * c, 0.5)) / (2 * a) + 0.0000001)
+    root2 = math.floor((-b - pow(b * b - 4 * a * c, 0.5)) / (2 * a) - 0.0000001)
+
+    return root2 - root1 + 1
+
+
 p1 = 1
 for time_limit, distance in records:
-    p1 *= race(int(time_limit), int(distance))
+    p1 *= count_brutal(int(time_limit), int(distance))
 
-p2 = race(p2_time, p2_distance)
+# p2 = count_brutal(p2_time, p2_distance)
+p2 = count_quadratic(p2_time, p2_distance)
 
 print(f"Part 1: {p1}")
 print(f"Part 2: {p2}")
